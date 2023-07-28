4402e1431c1da0fc5c03e7a7af5d36b98e71ea1a
OURAGHI <ouraghiahmedelamine@gmail.com>
2023-07-28 11:20:51 +0100

    including the leaflet map

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+// const map = document.querySelector('#map');
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -15,8 +16,22 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
+      console.log(position);
       const { latitude, longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${longitude},${latitude}`);
+      // leaflet api
+      //'map' must be the ID name of an element in our html
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert("Can't fincde the location");
